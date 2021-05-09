3b7ee44c8882accd2498b36f2b0034bc5fe26722
Nithyanand K N <knnithyanand@hotmail.com>
2021-05-09 07:26:52 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Client Info
+
+Frappe App to gather client details such as device, browser and location information.
+
+#### License
+
+MIT
---
+++ client_info/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Client Info",
+			"color": "grey",
+			"icon": "octicon octicon-beaker",
+			"type": "module",
+			"label": _("Client Info")
+		}
+	]
